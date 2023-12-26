acbb33fbf1328a4bfced31e5ad27bec76aa74033
Malith Dilshan <79596630+Malith-19@users.noreply.github.com>
2023-12-26 17:57:33 +0530

    Add the fast API back-end.

---
+++ CV Book Data API.py
@@ -0,0 +1,54 @@
+import os, glob
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+def get_photo_path(index):
+    exts = ["jpg", "jpeg", "JPG", "JPEG"]
+    for ext in exts:
+        path1 = os.path.join("Photos", f"{index}.{ext}")
+        if os.path.exists(path1):
+            return path1
+    return None
+
+def get_data_file_path():
+    path = os.path.join("Json", "info-*.json")
+    files = glob.glob(path)
+    files.sort(key=os.path.getmtime, reverse=True)
+    if len(files) == 0:
+        return None
+    return files[0]
+
+@app.get("/")
+def root():
+    return "Carriers Fair 2023 - CV Book Data API"
+
+@app.get("/data")
+def data():
+    path = get_data_file_path()
+    if path is None:
+        raise HTTPException(status_code=404, detail="Data file not found")
+    else:
+        path = os.path.abspath(path)
+        return FileResponse(path)
+
+@app.get("/photo/{index}")
+def photo(index, q = None):
+    path = get_photo_path(index)
+    if path is None:
+        path = get_photo_path("180XXX")
+        return FileResponse(path)
+    else:
+        path = os.path.abspath(path)
+        return FileResponse(path)
+    
